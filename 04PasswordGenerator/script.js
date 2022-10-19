4eb9a8b6a071b5ee4ad3ed7e12abdee8d6fdc373
prajwal zingare <prajwalzingre@gmail.com>
2022-10-19 08:11:33 +0530

    joke generator done

--- 04PasswordGenerator/script.js
+++ 04PasswordGenerator/script.js
@@ -1,39 +1,29 @@
-const resultEl = document.getElementById('result')
-const lengthEl = document.getElementById('length')
-const uppercaseEl = document.getElementById('uppercase')
-const lowercaseEl = document.getElementById('lowercase')
-const numbersEl = document.getElementById('numbers')
-const symbolsEl = document.getElementById('symbols')
-const generateEl = document.getElementById('generate')
-const clipboardEl = document.getElementById('clipboard')
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
 
 const randomFunc = {
-    lower: getRandomLower,
-    upper: getRandomUpper,
-    number: getRandomNumber,
-    symbol: getRandomSymbol
-}
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
 
-clipboardEl.addEventListener('click', () => {
-    
-})
+clipboardEl.addEventListener("click", () => {});
 
-generateEl.addEventListener('click', () => {
-    
-})
+generateEl.addEventListener("click", () => {});
 
-function generatePassword(lower, upper, number, symbol, length) {
-    
-}
+function generatePassword(lower, upper, number, symbol, length) {}
 
-function getRandomLower() {
-}
+function getRandomLower() {}
 
-function getRandomUpper() {
-}
+function getRandomUpper() {}
 
-function getRandomNumber() {
-}
+function getRandomNumber() {}
 
-function getRandomSymbol() {
-}
+function getRandomSymbol() {}
